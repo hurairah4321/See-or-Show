339c0e9b1f0d58106c0a8504d2b6ea74174e19f2
hurairah4321 <151510774+hurairah4321@users.noreply.github.com>
2024-01-06 18:12:47 +0500

    Update main.py
    Change validation

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, validator  # Importing validator here
-from function import main_process  # Importing main_process function
-import validators  # Import the validators module
+from pydantic import BaseModel, validator
+from function import main_process
+import validators
 import uvicorn
+
 app = FastAPI()
 
 @app.get("/")
@@ -14,7 +15,7 @@ class ImageRequest(BaseModel):
     tileImage: str
 
     # Custom validator for image URLs
-    @validator('floorImage', 'tileImage', each_item=True)  # Added each_item=True for individual validation
+    @validator('floorImage', 'tileImage')
     def url_must_be_valid(cls, v):
         if not validators.url(v):
             raise ValueError('Please provide a valid URL')
@@ -24,10 +25,7 @@ class ImageRequest(BaseModel):
 async def perform_image_processing(request: ImageRequest):
     try:
         # If we're here, the URLs are valid
-        uploaded_image_url = main_process(request.floorImage, request.tileImage)  # Using main_process function
+        uploaded_image_url = main_process(request.floorImage, request.tileImage)
         return {"uploaded_image_url": uploaded_image_url}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=5173, reload=True)
